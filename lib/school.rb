12145760fb49dcb55c2576b4886310f2200c0b9d
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-18 13:47:45 -0500

    commit tree 51bd5e0e33e85c4657dd3d82ebf57f2a69116ac1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+    attr_accessor :roster
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if 
+            @roster.has_key?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.map do |grade, name|
+           name.sort!
+        end
+        @roster
+        end
+    
+end
+
